7acdea562347ca337a8bb44c7e52ecef7740b5d2
No <coding.plswork@gmail.com>
2026-03-15 13:29:36 -0500

    think I'v fixed it

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setup(
     name="error_values",
-    version="0.1.7",
+    version="0.1.8",
     packages=find_packages(),
     long_description=long_description,
     long_description_content_type='text/markdown')

e765476d9aed9edc0bca95eeefa86da6ffd61a0d
No <coding.plswork@gmail.com>
2026-03-15 10:30:57 -0500

    for linting

--- error_values/setup.py
+++ error_values/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
 from pathlib import Path
 this_directory = Path(__file__).parent
@@ -9,5 +9,4 @@ setup(
     version="0.1.4",
     packages=find_packages(),
     long_description=long_description,
-    long_description_content_type='text/markdown'
-    )
+    long_description_content_type='text/markdown')
